c1836ef51a112ec1379c6149b4dcc0d72447c96e
Dennis Pierre <a00823797@dtc01l0860-06.ad.bcit.ca>
2018-01-27 17:20:52 -0800

    the whole thing

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component, Image } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+            <header className="head">Who we are</header>
+                <div className="boxone">
+                    <p className="textone">
+                    Dennis Pierre – Napoleon, don’t be jealous that I’ve been chatting online with babes all day. Besides, we both know that I’m training to be a cage fighter.
+                    </p>
+                </div>
+                <div className="boxtwo">
+                    <p className="texttwo">
+                    Christen Mangibin – An apple a day keeps anyone away, if you throw hard enough.
+                    </p>
+                </div>
+        </div>
+        
+    );
+  }
+}
+
+export default App;
